7e3d656905c667f88416285c83325af8894c0c74
Scott O'Malley <scottyomalley@gmail.com>
2017-08-21 09:51:11 +0100

    Added form validation functions to form functions

--- form_functions.php
+++ form_functions.php
@@ -88,3 +88,149 @@ function iff_renewal_form_post_submission($form)
         GFAPI::delete_entry($value['id']);
     }
 }
+
+
+<?php
+
+add_filter('gform_pre_submission_32', 'populateIFFNumber');
+function populateIFFNumber( $form ) {
+
+  $iff_number = rgpost( "input_19" );
+  debug_to_console($iff_number);
+
+  if($iff_number == null) {
+    debug_to_console("iff_number");
+
+    $IFFLicenseNumber =  intval(get_option( "new_iff_licence_numbers"));
+
+    $_POST['input_19'] = strval($IFFLicenseNumber)."IRL";
+
+    $IFFLicenseNumber += 1;
+
+    update_option("new_iff_licence_numbers", strval($IFFLicenseNumber));
+  }
+}
+
+add_filter( 'gform_validation_32', 'form_32_validate_input' );
+function form_32_validate_input( $validation_result ) {
+  $form = $validation_result['form'];
+  $current_page = rgpost( 'gform_source_page_number_' . $form['id'] ) ? rgpost( 'gform_source_page_number_' .   $form['id'] ) : 1;
+
+  if($current_page == 1){
+   form_32_reset_details();
+  }
+  else if ($current_page == 2) {
+    $formID = 22;
+
+    $email = rgpost( "input_37" );
+    $iff_number = rgpost("input_36");
+    $hash = rgpost("input_38");
+
+    $search_criteria = array(
+      'field_filters' => array(
+            'mode' => 'all',
+            array(
+                'key' => '1',
+                'value' => $email
+            ),
+            array(
+                'key' => '2',
+                'value' => $iff_number
+            ),
+            array(
+                'key' => '3',
+                'value' => $hash
+            )
+        )
+    );
+
+    $detailsFound = GFAPI::get_entries($formID, $search_criteria);
+
+    $is_valid = (count($detailsFound) == 1 ? true : false);
+
+    debug_to_console($is_valid);
+
+    $validation_result['is_valid'] = $is_valid;
+
+    if($is_valid) {
+
+      $fencer = get_fencer_details();
+
+      debug_to_console(json_encode($fencer));
+
+      $_POST['input_1_2'] = $fencer['1.2'];
+      $_POST['input_1_3'] = $fencer['1.3'];
+      $_POST['input_1_6'] = $fencer['1.6'];
+      $_POST['input_19'] =  $fencer['19'];
+    }
+    else{
+       foreach( $form['fields'] as &$field ) {
+        if($field['id'] == 36){
+          $field->failed_validation = true;
+          $field->validation_message = "The details entered don't match our records, please check them and try again.";
+        }
+        else if ($field['id'] == 37){
+          $field->failed_validation = true;
+          $field->validation_message = "The details entered don't match our records, please check them and try again.";
+        }
+        else if ($field['id'] == 38){
+          $field->failed_validation = true;
+          $field->validation_message = "The details entered don't match our records, please check them and try again.";
+        }
+       }
+    }
+
+  }
+
+  return $validation_result;
+}
+
+function get_fencer_details(){
+
+    $membershipForm1 = 1;
+    $membershipForm21 = 21;
+
+    $search_criteria = array(
+        'field_filters' => array(
+            'mode' => 'all',
+            array(
+                'key'   => '19',
+                'value' => $iff_number
+            )
+        )
+    );
+
+    $fencerQuery = GFAPI::get_entries($membershipForm21, $search_criteria);
+    if(count($fencerQuery) > 0){
+        return $fencerQuery[0];
+    } else {
+        $fencerQuery = GFAPI::get_entries($membershipForm1, $search_criteria);
+        return $fencerQuery[0];
+    }
+
+
+
+
+
+}
+
+function form_32_reset_details(){
+    $_POST['input_1_2'] = '';
+    $_POST['input_1_3'] = '';
+    $_POST['input_1_6'] = '';
+    $_POST['input_19'] = '';
+    $_POST['input_37'] = '';
+    $_POST['input_38'] = '';
+    $_POST['input_36'] = '';
+}
+
+
+function debug_to_console( $data ) {
+
+    if ( is_array( $data ) )
+        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+    else
+        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+    echo $output;
+}
