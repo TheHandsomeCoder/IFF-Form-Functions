1dccfe73221c5d802b0d80fd1e32acfc1fb72eed
Scott O'Malley <scottyomalley@gmail.com>
2017-08-21 13:29:46 +0100

    Bump Version Number

--- form_functions.php
+++ form_functions.php
@@ -3,7 +3,7 @@
 Plugin Name: IFF Membership Form Functions
 Description: Plugin for abstracting custom form functions
 Author: Scott O'Malley
-Version: 1.5.2
+Version: 1.5.3
 GitHub Plugin URI: https://github.com/TheHandsomeCoder/IFF-Form-Functions
 GitHub Branch:     master
 */
